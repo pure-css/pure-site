00b6227ba8665a99313b0aebd67cc46f2800e465
Tilo Mitra <tilomitra@gmail.com>
2013-03-06 16:43:24 -0500

    add normalize routes

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.locals({
     nav: [
         {id: 'home',   url: '/',        label: 'Home'},
         {id: 'grids',  url: '/grids/',  label: 'Grids'},
+        {id: 'base',   url: '/base/',   label: 'Base'},
         {id: 'forms',  url: '/forms/',  label: 'Forms'},
         {id: 'tables', url: '/tables/', label: 'Tables'},
         {id: 'lists',  url: '/lists/',  label: 'Navigation'}
@@ -62,6 +63,7 @@ if (config.isDevelopment) {
 // -- Routes -------------------------------------------------------------------
 
 app.get('/',        routes.home);
+app.get('/base/',   routes.base);
 app.get('/grids/',  routes.grids);
 app.get('/forms/',  routes.forms);
 app.get('/tables/', routes.tables);
