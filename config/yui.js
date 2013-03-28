a04c705cac33507e5126f67b46ee11efe68603f6
Tilo Mitra <tilomitra@gmail.com>
2013-03-28 11:22:51 -0400

    update gallery build tag

--- config/yui.js
+++ config/yui.js
@@ -2,7 +2,7 @@ var isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = Object.freeze({
     version: '3.9.0',
-    gallery: 'gallery-2013.03.20-19-59',
+    gallery: 'gallery-2013.03.27-22-06',
     config: JSON.stringify({
         combine: isProduction,
         filter : isProduction ? 'min' : 'raw',
