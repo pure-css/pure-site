714be643362abb261929824c1893832abb27dc8f
Tilo Mitra <tilomitra@gmail.com>
2013-07-31 13:36:05 -0400

    redirect /updates/ to pure blog (via 302 permanent redirect)

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.get('/combo/:version', [
 ]);
 
 // Redirects
-app.get('/updates/', routes.redirect('https://github.com/yui/pure/releases'));
+app.get('/updates/', routes.redirect('http://blog.purecss.io/'));
 
 // -- Exports ------------------------------------------------------------------
 module.exports = app;
