f86dc02c023a91dd9f76bb6a7c340c4adddf7243
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-08-22 01:50:43 -0400

    Remove redundant view name and use the route's annotated name

--- app.js
+++ app.js
@@ -96,16 +96,16 @@ function routePage(name, path, label, callbacks) {
 }
 
 // Basic docs pages.
-routePage('home',      '/',                        routes.render('home'));
-routePage('base',      '/base/',      'Base',      routes.render('base'));
-routePage('grids',     '/grids/',     'Grids',     routes.render('grids'));
-routePage('forms',     '/forms/',     'Forms',     routes.render('forms'));
-routePage('buttons',   '/buttons/',   'Buttons',   routes.render('buttons'));
-routePage('tables',    '/tables/',    'Tables',    routes.render('tables'));
-routePage('menus',     '/menus/',     'Menus',     routes.render('menus'));
-routePage('customize', '/customize/', 'Customize', routes.render('customize'));
-routePage('extend',    '/extend/',    'Extend',    routes.render('extend'));
-routePage('layouts',   '/layouts/',   'Layouts',   routes.render('layouts'));
+routePage('home',      '/',                        routes.render());
+routePage('base',      '/base/',      'Base',      routes.render());
+routePage('grids',     '/grids/',     'Grids',     routes.render());
+routePage('forms',     '/forms/',     'Forms',     routes.render());
+routePage('buttons',   '/buttons/',   'Buttons',   routes.render());
+routePage('tables',    '/tables/',    'Tables',    routes.render());
+routePage('menus',     '/menus/',     'Menus',     routes.render());
+routePage('customize', '/customize/', 'Customize', routes.render());
+routePage('extend',    '/extend/',    'Extend',    routes.render());
+routePage('layouts',   '/layouts/',   'Layouts',   routes.render());
 
 // Layout examples.
 app.get('/layouts/:layout/', routes.layout('layouts/'));
