c4cd6f3cac897eb2da918f3d83c363dbf6a406ea
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-08-30 11:42:34 -0400

    Upgrade YUI to 3.12.0

--- config/yui.js
+++ config/yui.js
@@ -1,7 +1,7 @@
 var isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = Object.freeze({
-    version: '3.10.1',
+    version: '3.12.0',
 
     config: {
         gallery: 'gallery-2013.03.27-22-06',
