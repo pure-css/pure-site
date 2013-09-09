be7d6ecab6b71ad40750fa26b72cc1992c315427
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-09-09 12:51:29 -0400

    Bump Pure version to v0.3.0

--- config/pure.js
+++ config/pure.js
@@ -4,7 +4,7 @@ var path  = require('path'),
     isProduction = process.env.NODE_ENV === 'production',
     isPureLocal, bowerrc, bower;
 
-exports.version   = '0.3.0-rc-3';
+exports.version   = '0.3.0';
 exports.filesizes = require('./filesizes');
 exports.modules   = ['base', 'grids', 'forms', 'buttons', 'tables', 'menus'];
 
