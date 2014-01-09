a92223a282e6e39901ad25bf7b5ad622514b9860
Tilo Mitra <tilomitra@gmail.com>
2014-01-09 12:18:37 -0500

    Update Gruntfile, main.handlebars, and package.json for oldIE mobile-first support

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,17 +5,27 @@ module.exports = function (grunt) {
             units: [5, 24],
             options: {
                 mediaQueries: {
-                    med : 'screen and (min-width: 48em)', //approx 767px at 16px base font
+                    med : 'screen and (min-width: 40em)', //approx 767px at 16px base font
                     lrg : 'screen and (min-width: 75em)' //approx 1200px at 16px base font
                 }
 
+            }
+        },
+
+        stripmq: {
+            all: {
+                files: {
+                    'public/css/responsive-grid-old-ie.css': ['public/css/responsive-grid.css'],
+                    'public/css/main-old-ie.css': ['public/css/main.css']
+                }
             }
         }
     })
 
     // Local tasks.
     grunt.loadTasks('lib/tasks/');
+    grunt.loadNpmTasks('grunt-stripmq');
 
-    grunt.registerTask('default', 'grid_units');
+    grunt.registerTask('default', ['grid_units', 'stripmq']);
 };
 
