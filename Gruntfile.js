33fd2190656117cf31393940f60127d1f5424847
Tilo Mitra <tilomitra@gmail.com>
2014-01-09 13:49:11 -0500

    update rework-pure-grids, remove units from Gruntfile.js so we only generate MQs

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@ module.exports = function (grunt) {
     grunt.initConfig({
         grid_units: {
             dest : 'public/css/responsive-grid.css',
-            units: [5, 24],
             options: {
                 mediaQueries: {
                     med : 'screen and (min-width: 40em)', //approx 767px at 16px base font
