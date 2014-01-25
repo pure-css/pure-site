3e629081e0186633d08c5fb59d213d2047c92c8f
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-01-24 20:10:45 -0500

    Update YUI config for tooling modules

--- config/index.js
+++ config/index.js
@@ -1,19 +1,21 @@
+'use strict';
+
 var env  = process.env,
     path = require('path');
 
-module.exports = Object.freeze({
+module.exports = {
     env          : env.NODE_ENV,
     isDevelopment: env.NODE_ENV !== 'production',
     isProduction : env.NODE_ENV === 'production',
 
     port: env.PORT || 5000,
 
-    dirs: Object.freeze({
+    dirs: {
         pub     : path.resolve('build/public/'),
         views   : path.resolve('views/pages/'),
         layouts : path.resolve('views/layouts/'),
         partials: path.resolve('views/partials/')
-    }),
+    },
 
     version: require('../package').version,
 
@@ -22,4 +24,4 @@ module.exports = Object.freeze({
     ga       : 'UA-41480445-1',
     typekit  : 'gis6vng',
     html5shiv: '3.7'
-});
+};
