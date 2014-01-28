d1ba2c85717710a0cfd1a0693048addd108c2054
Tilo Mitra <tilomitra@gmail.com>
2014-01-28 17:00:50 -0500

    read query string and add sensible defaults

--- routes/start.js
+++ routes/start.js
@@ -1,6 +1,38 @@
 'use strict';
-
+var utils = require('../lib/utils');
+/*
+    Routes for /start/ could be any of the following:
+    * `/start/`
+    * `/start/?cols=6&med=48em&lrg=60em`
+    * `/start/?cols=6&sm=screen and (min-device-width: 480px)`
+*/
 module.exports = function (req, res, next) {
-    res.expose(req.query, 'app.start.query');
+
+
+    var query = utils.extend({
+        cols: '5,24',
+        fonts: 'sans-serif',
+        prefix: '.pure-u-',
+    }, req.query);
+
+    //If no media queries were specified, we'll create some defaults.
+    if (!hasMQ(query)) {
+        query.med = 'screen and (min-width: 48em)';
+        query.lrg = 'screen and (min-width: 60em)';
+    }
+
+    res.expose(query, 'app.start.query');
     res.render('start');
 };
+
+
+function hasMQ (obj) {
+    var o = utils.extend({}, obj);
+    delete o.cols;
+    delete o.fonts;
+    delete o.prefix;
+    if (Object.getOwnPropertyNames(o).length === 0) {
+        return false;
+    }
+    return o;
+}
