8966f570eadc1739f8615203823ee19982e61037
Tilo Mitra <tilomitra@gmail.com>
2014-02-20 16:59:54 -0500

    beef up grid-model with stringify() method, attribute validations

--- public/js/models/grid-model.js
+++ public/js/models/grid-model.js
@@ -14,6 +14,26 @@ YUI.add('grid-model', function (Y) {
             this.set('mediaQueries', mq);
         },
 
+        stringify: function () {
+
+            var o = this.toJSON(),
+                mq = this.get('mediaQueries');
+
+            delete o.mediaQueries;
+            delete o.id;
+
+            Y.Object.each(o, function (val, key) {
+                if (!val) {
+                    delete o[key];
+                }
+            });
+
+            mq.each(function (val) {
+                o[val.get('id')] = val.get('mq');
+            });
+            return Y.QueryString.stringify(o);
+        },
+
         generate: function () {
             var mediaQueries = this.get('mediaQueries'),
                 css = '';
@@ -26,7 +46,32 @@ YUI.add('grid-model', function (Y) {
         }
 
     }, {
-        ATTRS: {}
+        ATTRS: {
+            cols: {
+                setter: function (val) {
+                    return parseInt(val, 10);
+                },
+
+                validator: function (val) {
+                    //no value is fine
+                    if (!val || (val <= app.start.limits.cols.max
+                        && val >= app.start.limits.cols.min)) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+
+            prefix: {
+                validator: function (val) {
+                    if (!val || (val.length <= app.start.limits.prefix.max
+                        && val.length >= app.start.limits.prefix.min)) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        }
     });
 
 }, '0.0.1', {
@@ -34,7 +79,8 @@ YUI.add('grid-model', function (Y) {
         'model',
         'mq-model-list',
         'rework',
-        'rework-pure-grids'
+        'rework-pure-grids',
+        'querystring'
     ]
 });
 
