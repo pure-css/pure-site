e4cd5df2744dbbc0c44a3d5b78581741a7437375
Tilo Mitra <tilomitra@gmail.com>
2014-05-20 19:33:32 -0400

    Revert "add conditional in html.handlebars based on whether grid-old-ie.css exists"
    This reverts commit 4530bfba9dd487daf34f513454243574f1e6637d.

--- public/js/home.js
+++
@@ -1,8 +0,0 @@
-YUI().use('node', function (Y) {
-    var slider = Y.one('#layout-slider'),
-        wrapper = Y.one('#layout-wrapper');
-
-    slider.on('change', function (e) {
-        wrapper.setStyle('width', e._event.srcElement.valueAsNumber + 'px');
-    });
-});
