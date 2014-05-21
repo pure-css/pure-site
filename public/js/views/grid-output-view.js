57a3882382b225a7e5ff029db092fc25ed435611
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-05-20 20:11:55 -0400

    Properly render Get Started conditions
    This fixes the rendering of no-MQs and default MQs conditions for the HTML template on the Get Started page.

--- public/js/views/grid-output-view.js
+++ public/js/views/grid-output-view.js
@@ -6,15 +6,16 @@ export default Base.create('grid-output-view', GridTabView, [], {
     render: function () {
         var model     = this.get('model'),
             container = this.get('container'),
+            hasMQs    = !!model.get('mediaQueries').size(),
             css       = model.get('css'),
             cssOldIE  = model.get('cssOldIE');
 
         container.one('#html code').removeClass('rainbow').setHTML(
             Escape.html(this.get('htmlTemplate')({
-                pure          : this.get('pure'),
-                needsOldIECSS : !!model.get('mediaQueries').size(),
-                css           : css,
-                cssOldIE      : cssOldIE
+                pure    : this.get('pure'),
+                needsCSS: css || hasMQs,
+                css     : css,
+                cssOldIE: cssOldIE
             }))
         );
 
